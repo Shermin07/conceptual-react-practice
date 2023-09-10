1335e889d6399d49c657c593618d5895aa35c0b2
Shermin Sultana <rima64549@gmail.com>
2023-09-11 01:46:13 +0600

    add

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Country from "../Country/Country";
+import "./countries.css"
 
 
 const Countries = () => {
@@ -14,10 +15,12 @@ useEffect(()=>{
     return (
         <div>
             <h1>Countries:{countries.length}</h1>
-            {
+          <div className="country-container">
+          {
                 countries.map(country => 
                 <Country key={country.cca3} country={country}></Country>)
             }
+          </div>
             
         </div>
         
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,13 +1,16 @@
 
 import "./Country.css"
 const Country = ({country}) => {
-   const {name,flags} = country;
+   const {name,flags,population,area,cca3} = country;
 
     return (
         <div className="country">
           <h3>Name:   {name.common}</h3>  
-          <h3>Flags:
-             <img src={flags.png}></img></h3>
+          
+             <img src={flags.png}></img>
+             <h3>Populations: {population}</h3>
+             <h3>Area: {area}</h3>
+             <p><small>Code: {cca3}</small></p>
         </div>
     );
 };
